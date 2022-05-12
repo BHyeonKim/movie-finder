4f1c867c5674a177f8ff580a7b62349d12f69361
김보현 <kimbohyeon@gimbohyeon-ui-MacBookPro.local>
2022-05-13 00:33:52 +0900

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
+import { RecoilRoot } from 'recoil';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
 import SearchBar from './components/SearchBar/SearchBar';
 import MovieList from './components/MovieList/MovieList';
+import Bookmark from './components/Bookmark/Bookmark';
 import Tab from './components/Tab/Tab';
 
 import classes from './App.module.scss';
-import { RecoilRoot } from 'recoil';
 
 const App = () => {
   return (
     <div className={classes.app}>
       <RecoilRoot>
-        <SearchBar />
-        <MovieList />
-        <Tab />
+        <Router>
+          <Routes>
+            <Route path='/bookmark' element={<Bookmark />} />
+            <Route
+              path='/'
+              element={
+                <>
+                  <SearchBar />
+                  <MovieList />
+                </>
+              }
+            />
+            <Route path='*' element={<Navigate replace to='/' />} />
+          </Routes>
+          <Tab />
+        </Router>
       </RecoilRoot>
     </div>
   );
