9866579cf593c07f04bb7186e1988eca727d7e19
김보현 <kimbohyeon@gimbohyeon-ui-MacBookPro.local>
2022-05-13 00:33:52 +0900

    add routes

---
+++ src/components/Bookmark/Bookmark.tsx
@@ -0,0 +1,7 @@
+import classes from './Bookmark.module.scss';
+
+const Bookmark = () => {
+  return <div />;
+};
+
+export default Bookmark;
--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -1,25 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import { BookMark, Search } from '../../assets/svgs';
 import classes from './Tab.module.scss';
 
 const Tab = () => {
   const [isClicked, setIsClicked] = useState(false);
+  const { pathname } = useLocation();
+
+  // To set slider position
+  useEffect(() => {
+    if (pathname === '/') setIsClicked(false);
+    if (pathname === '/bookmark') setIsClicked(true);
+  }, [pathname, setIsClicked]);
 
   const toggleChangeHandler = () => {
     setIsClicked((prev) => !prev);
   };
 
   return (
-    <button type='button' className={classes.tab} onClick={toggleChangeHandler}>
+    <Link type='button' to={isClicked ? '/' : '/bookmark'} className={classes.tab} onClick={toggleChangeHandler}>
       <div className={classes.tab__icon}>
         <Search className={classes.search} />
       </div>
       <div className={classes.tab__icon}>
         <BookMark className={classes.bookmark} />
       </div>
-      <div className={isClicked ? classes.slider : classes['slider--active']} />
-    </button>
+      <div className={isClicked ? classes['slider--active'] : classes.slider} />
+    </Link>
   );
 };
 
