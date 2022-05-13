444830805b3dbf01533a8b2f671eb041c997c7d3
김보현 <kimbohyeon@gimbohyeon-ui-MacBookPro.local>
2022-05-13 20:00:02 +0900

    local storage에서 bookmark 불러오기 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,47 @@
-import { RecoilRoot } from 'recoil';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
 
 import SearchBar from './components/SearchBar/SearchBar';
 import MovieList from './components/MovieList/MovieList';
 import Bookmark from './components/BookmarkList/BookmarkList';
 import Tab from './components/Tab/Tab';
 
+import { bookMarkState } from './state/moviesState';
+
 import classes from './App.module.scss';
+import { MovieItemDefinition } from './types/movie.d';
+import { useEffect } from 'react';
 
 const App = () => {
+  const setBookmark = useSetRecoilState(bookMarkState);
+
+  // 처음 localstorage에 들어있는 bookmark 불러오기
+  useEffect(() => {
+    const storedBookmarks: MovieItemDefinition[] = JSON.parse(localStorage.getItem('movies') || '[]');
+    if (storedBookmarks.length === 0) {
+      return;
+    }
+    setBookmark([...storedBookmarks]);
+  }, [setBookmark]);
+
   return (
     <div className={classes.app}>
-      <RecoilRoot>
-        <Router>
-          <Routes>
-            <Route path='/bookmark' element={<Bookmark />} />
-            <Route
-              path='/'
-              element={
-                <>
-                  <SearchBar />
-                  <MovieList />
-                </>
-              }
-            />
-            <Route path='*' element={<Navigate replace to='/' />} />
-          </Routes>
-          <Tab />
-        </Router>
-      </RecoilRoot>
+      <Router>
+        <Routes>
+          <Route path='/bookmark' element={<Bookmark />} />
+          <Route
+            path='/'
+            element={
+              <>
+                <SearchBar />
+                <MovieList />
+              </>
+            }
+          />
+          <Route path='*' element={<Navigate replace to='/' />} />
+        </Routes>
+        <Tab />
+      </Router>
     </div>
   );
 };
