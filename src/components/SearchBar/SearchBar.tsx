5a140bf5c097808cb40df0cda078291c8a36ee9a
김보현 <kimbohyeon@gimbohyeon-ui-MacBookPro.local>
2022-05-14 02:34:57 +0900

    검색결과가 없을시 메세지 표시

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,34 +1,55 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useCallback, useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { getMoviesFromApi } from '../../services/movie';
 import { keywordState, moviesState } from '../../state/moviesState';
+import MessageBox from '../MessageBox/MessageBox';
 
 import classes from './SearchBar.module.scss';
 
 const SearchBar = () => {
   const [text, setText] = useState('');
+  const [resultState, setResultState] = useState('검색결과가 없습니다.');
   const setMovies = useSetRecoilState(moviesState);
   const setKeyword = useSetRecoilState(keywordState);
 
-  const submitHandler = async (e: FormEvent) => {
-    e.preventDefault();
-    setMovies([]);
+  const submitHandler = useCallback(
+    async (e: FormEvent) => {
+      e.preventDefault();
+      setMovies([]);
 
-    if (text.trim().length === 0) return;
+      if (text.trim().length === 0) return;
 
-    setMovies(await getMoviesFromApi(text, 1));
-    setKeyword(text);
+      try {
+        setMovies(await getMoviesFromApi(text, 1));
+      } catch (error) {
+        if (error instanceof Error) {
+          setResultState(error.message);
+          return;
+        }
+        return;
+      }
+      setKeyword(text);
 
-    setText('');
-  };
+      setText('');
+      setResultState('');
+    },
+    [setMovies, setKeyword, text]
+  );
 
-  const inputTextHandler = (e: FormEvent<HTMLInputElement>) => {
+  const inputTextHandler = useCallback((e: FormEvent<HTMLInputElement>) => {
     setText(e.currentTarget.value);
-  };
+  }, []);
 
   return (
     <form onSubmit={submitHandler}>
-      <input type='text' onChange={inputTextHandler} className={classes.input} value={text} />
+      <input
+        type='text'
+        onChange={inputTextHandler}
+        className={classes.input}
+        value={text}
+        placeholder='What is your favorite movie?'
+      />
+      {resultState && <MessageBox message={resultState} />}
     </form>
   );
 };
